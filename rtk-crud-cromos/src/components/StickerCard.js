0cb84b9b581a5a1e219571a5f7df4fba194e3b51
ajrosas <alejandrojrosas2001@gmail.com>
2022-11-06 11:41:48 -0400

    Cambio de Posición de Botones de Editar/Eliminar

--- rtk-crud-cromos/src/components/StickerCard.js
+++ rtk-crud-cromos/src/components/StickerCard.js
@@ -17,9 +17,15 @@ function StickerCard() {
         <div className='grid grid-cols-3 gap-7'>
             {stickers.map(sticker => (
                 <div key={sticker.id} className='bg-slate-400 p-4 rounded-md'>
+                    <h3 className='text-sm'>Nombre: {sticker.playerName}</h3>
+                    <p className='text-sm'>Equipo: {sticker.team}</p>
+                    <p className='text-sm'>País: {sticker.country}</p>
+                    <p className='text-sm'>Posición: {sticker.position}</p>
+                    <p className='text-sm'>Altura: {sticker.height}</p>
+                    <p className='text-sm'>Peso: {sticker.weight}</p>
+                    <p className='text-sm'>Probabilidad de Aparición: {sticker.appearanceRate}</p>
                     <header className='flex justify-between'>
-                        <h3 className='text-sm'>Nombre: {sticker.playerName}</h3>
-                        <div className='flex gap-x-1'>
+                        <div className='flex grid grid-cols-2 gap-1'>
                             <Link
                                 to={`/edit-sticker/${sticker.id}`}
                                 className='bg-green-500 px-2 py-1 text-xs rounded-md hover:bg-green-600'
@@ -34,12 +40,6 @@ function StickerCard() {
                             </button>
                         </div>
                     </header>
-                    <p className='text-sm'>Equipo: {sticker.team}</p>
-                    <p className='text-sm'>País: {sticker.country}</p>
-                    <p className='text-sm'>Posición: {sticker.position}</p>
-                    <p className='text-sm'>Altura: {sticker.height}</p>
-                    <p className='text-sm'>Peso: {sticker.weight}</p>
-                    <p className='text-sm'>Probabilidad de Aparición: {sticker.appearanceRate}</p>
                 </div>
             ))}
         </div>
