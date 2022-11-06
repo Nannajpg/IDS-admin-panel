c7297bf586775ba55c169158afbfc916303e72ab
Aurimart <aurimart.liliana@gmail.com>
2022-11-06 03:19:53 -0400

    save new sticker

--- rtk-crud-cromos/src/features/stickers/stickerSlice.js
+++ rtk-crud-cromos/src/features/stickers/stickerSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getAllStickers, saveSticker,editSticker, deletSticker} from '../../services/axios';
 
 const initialState = []
 
@@ -6,8 +7,21 @@ export const stickerSlice = createSlice({
     name: 'stickers',
     initialState,
     reducers: {
-        createSticker: (state, action) => {
-            state.push(action.payload)
+        createSticker: async (state, action) => {
+            const {id, playerName, team, country, position, height, weight, appearanceRate} = action.payload;
+            const sticker={
+                "id": id,
+                "playerName": playerName,
+                "team": team,
+                "country": country,
+                "position": position,
+                "img": "",
+                "height": height,
+                "weight": weight,
+                "appearanceRate": appearanceRate
+            };
+            await saveSticker(sticker);
+            state.push(action.payload);
         },
         updateSticker: (state, action) => {
             const {id, playerName, team, country, position, height, weight, appearanceRate} = action.payload
@@ -20,17 +34,21 @@ export const stickerSlice = createSlice({
                 foundSticker.height = height
                 foundSticker.weight = weight
                 foundSticker.appearanceRate = appearanceRate
+                editSticker(foundSticker);
             }
         },
         deleteSticker: (state, action) => {
             const stickerFound = state.find(sticker => sticker.id === action.payload)
             if (stickerFound) {
+                deletSticker(stickerFound)
                 state.splice(state.indexOf(stickerFound), 1)
             }
         },
         
     },
 })
+
+
 
 export const { createSticker, updateSticker, deleteSticker } = stickerSlice.actions
 
