62170b27bebc4a30b4cacc30a415079bbf4f1b6f
GloodBuster <josevglod@gmail.com>
2022-11-14 19:40:35 -0400

    Icono de barra lateral acomodado

--- src/components/LateralBar.jsx
+++ src/components/LateralBar.jsx
@@ -10,8 +10,8 @@ function LateralBar({ isVisible, hideLateralBar }) {
       {isVisible && (
         <div className="w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-50 flex items-center">
           <div className="md:w-1/6 md:min-h-screen bg-zinc-800 p-5">
-            <div className="font-sans">
-              <button className="ml-2 mb-4 hover:bg-zinc-900 rounded-full p-2" onClick={() => hideLateralBar(isVisible)}>
+            <div className="font-sans h-screen" >
+              <button className="my-4 hover:bg-zinc-900 rounded-full p-2" onClick={() => hideLateralBar(isVisible)}>
                 <CgMenu size="2rem" />
               </button>
               <div>
