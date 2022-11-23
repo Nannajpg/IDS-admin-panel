a56fcf15733fb451ee93b401fa4f0fed2154f0a7
ajrosas <alejandrojrosas2001@gmail.com>
2022-11-22 20:12:18 -0400

    Casi todos los cambios de formulario

--- src/cores/stickers/StickerForm.js
+++ src/cores/stickers/StickerForm.js
@@ -53,36 +53,39 @@ function StickerForm() {
         <div className='flex items-center h-screen'>
             <form onSubmit={handleSubmit} className='bg-slate-300 max-w-sm p-4 rounded-md grid grid-cols-2'>
 
-                <label htmlFor='playerName' className='block text-xs font-bold mb-2'>Nombre de Jugador:</label>
+                <label for='playerName' className='block text-xs font-bold mb-2'>Nombre de Jugador:</label>
                 <input
-                    name='playerName'
+                    id='playerName'
                     type='text'
                     onChange={handleChange}
                     value={sticker.playerName}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
                 />
-                <label htmlFor='team' className='block text-xs font-bold mb-2'>Equipo al que Juega:</label>
+
+                <label for='team' className='block text-xs font-bold mb-2'>Equipo al que Juega:</label>
                 <input
-                    name='team'
+                    id='team'
                     type='text'
                     onChange={handleChange}
                     value={sticker.team}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
                 />
-                <label htmlFor='country' className='block text-xs font-bold mb-2'>País de donde Proviene:</label>
+
+                <label for='country' className='block text-xs font-bold mb-2'>País de donde Proviene:</label>
                 <input
-                    name='country'
+                    id='country'
                     type='text'
                     onChange={handleChange}
                     value={sticker.country}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
                 />
-                <label htmlFor='position' className='block text-xs font-bold mb-2'>Posición:</label>
+
+                <label for='position' className='block text-xs font-bold mb-2'>Posición:</label>
                 <select
-                    name='position'
+                    id='position'
                     onChange={handleChange}
                     value={sticker.position}
                     className='w-full p-1 border border-gray-300 focus:border-blue-500 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
@@ -93,44 +96,55 @@ function StickerForm() {
                     <option value='MedioCentro'>MedioCentro</option>
                     <option value='Delantero'>Delantero</option>
                 </select>
-                <label htmlFor='height' className='block text-xs font-bold mb-2'>Altura (m):</label>
+
+                <label for='height' className='block text-xs font-bold mb-2'>Altura (cm):</label>
                 <input
-                    name='height'
+                    id='height'
                     type='number'
+                    step={0.1} min={1} max={272}
                     onChange={handleChange}
                     value={sticker.height}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
-                ></input>
-                <label htmlFor='weight' className='block text-xs font-bold mb-2'>Peso (Kg):</label>
+                />
+
+                <label for='weight' className='block text-xs font-bold mb-2'>Peso (Kg):</label>
                 <input
-                    name='weight'
+                    id='weight'
                     type='number'
+                    step={0.1} min={1} max={634}
                     onChange={handleChange}
                     value={sticker.weight}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
-                ></input>
-                <label htmlFor='appearanceRate' className='block text-xs font-bold mb-2'>Tasa de Aparición (%):</label>
+                />
+
+                <label for='appearanceRate' className='block text-xs font-bold mb-2'>Tasa de Aparición (%):</label>
                 <input
-                    name='appearanceRate'
+                    id='appearanceRate'
                     type='number'
+                    step={0.0001} min={0} max={1}
                     onChange={handleChange}
                     value={sticker.appearanceRate}
                     className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
                     required
-                ></input>
-                <label htmlFor='img' className='block text-xs font-bold mb-2'>
-                    Url de la Imagen:
+                />
+
+                <label className='block text-xs font-bold mb-2' for="file_input">
+                    Subir Archivo de Imagen
                 </label>
                 <input
-                    name='img'
-                    type='text'
+                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    aria-describedby="file_input_help"
+                    id="file_input"
+                    type="file"
+                    accept='.jpg, .jpeg, .png'
                     onChange={handleChange}
-                    value={sticker.img}
-                    className='w-full p-1 rounded-md bg-slate-400 mb-2 hover:bg-slate-500'
-                    required
                 />
+                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">
+                    Only PNG, JPG or JPEG
+                </p>
+
                 <div className='flex grid'>
                     <header className='mb-2'>
                         <Link to='/' className='flex justify-center w-full bg-red-500 py-1 hover:bg-red-600'>
