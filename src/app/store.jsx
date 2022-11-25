dc7fc4f0f8e15333672929402a53c2346ec83f24
Aurimart Garcia <aurimart.liliana@gmail.com>
2022-11-24 22:33:29 -0400

    empty branch

--- src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import adsReducer from "../features/ads/adSlice";
-
-export const store = configureStore({
-  reducer: {
-    ads: adsReducer
-  }
-});
