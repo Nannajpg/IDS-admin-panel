5404c84d5cf1293cafd83d168936dd46a4ab2d85
Yisustxz <jesusoortiz2410@gmail.com>
2023-01-08 19:53:54 -0400

    hecho el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,18 @@ import CreateFormTeam from "./cores/teams/CreateFormTeam";
 import EditFormTeam from "./cores/teams/EditFormTeam";
 import MatchesList from './cores/matches/MatchesList';
 import MatchCreateForm from './cores/matches/CreateForm';
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='bg-zinc-900 h-screen text-white'>
+      <Navbar/>
       <div className='flex items-center justify-center h-full'>
         <BrowserRouter>
         <Routes>
           <Route path="/" element= {<Login/>} />
+          
           <Route element={<Auth />}>
             <Route path="dashboard" element= {<Menu />} />
             <Route path="users/" element={<UsersList/>} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../cores/auth/assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        
+     <nav className="bg-[#EAEAEA] overflow-hidden border-gray-200 h-14 absolute w-full px-2 sm:px-4 py-2 dark:bg-gray-900">
+        <div className="container flex items-center justify-between h-full">
+              <div className="flex items-center">
+                 <img src={logo} alt="" className="h-14" />
+              </div>
+          </div>
+          <hr className="bg-[#CECECE] h-5"/>
+        </nav>
+
+    </div>
+
+  )
+}
+
+export default Navbar
