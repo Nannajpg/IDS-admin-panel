3041cc136ac3109806caae32aa054a06d630730e
Alejandro <alejandrojrosas2001@gmail.com>
2023-01-19 04:09:13 -0400

    cambios

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -1,30 +1,34 @@
 function SelectComponent({ label, placeholder, options, value, onChange }) {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
 
-    const handleChange = event => {
-        onChange(event.target.value);
-    }
+  return (
+    <>
+      <label className="block text-lg font-bold mb-2">{label}</label>
+      <select
+        className="w-full p-1 focus:border-blue-500 rounded-2xl bg-white mb-2 hover:bg-zinc-200"
+        onChange={handleChange}
+        placeholder={placeholder ? placeholder : ""}
+        defaultValue=""
+        required
+      >
+        <option value="" selected>
+          {placeholder}
+        </option>
 
-    return (
-        <>
-            <label className='block text-lg font-bold mb-2'>{ label }</label>
-            <select 
-                className="w-full p-1 focus:border-blue-500 rounded-2xl bg-white mb-2 hover:bg-zinc-200" 
-                onChange={handleChange} 
-                placeholder={placeholder ? placeholder : ''} 
-                defaultValue=""
-                required
-            >
-                <option value="" selected>{placeholder}</option>
-                
-                { options.map(option => 
-                <option 
-                    selected={option.id === value}
-                    value={option.id}
-                    key={option.id}
-                >{option.name}</option>) }
-            </select>
-        </>
-    )
+        {options.map((option) => (
+          <option
+            selected={option.id === value}
+            value={option.id}
+            key={option.id}
+          >
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
 }
 
 export default SelectComponent;
